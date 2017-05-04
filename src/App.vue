65ba6001bfb7347efe6d9ac8497cc44b33c45efb
hejin <286891132@qq.com>
2017-05-04 09:15:06 +0800

    '第一次框架上传'

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <ul id="tab">
+    	<li>
+    		<router-link to="home">首页</router-link>
+    	</li>
+    	<li>
+    		<router-link to="category">分类</router-link>
+    	</li>
+    	<li>
+    		<router-link to="shopcar">购物车</router-link>
+    	</li>
+    	<li>
+    		<router-link to="mine">我的</router-link>
+    	</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+html,body{width: 100%;height: 100%;}
+*{margin: 0;padding: 0;}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ul{list-style: none}
+#tab{position: fixed;bottom: 0;width: 100%;height: 10%;background: lightpink}
+#tab>li{float: left;width: 25%;}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="home">
+		<div id="home_main">
+			<!--<div id="home_search">
+				<form action="" method="post">
+					<img src="components/home/img/logo.png" alt="" id="logo" />
+					<a href="javascript:vido(0)" ui-sref='.search'>
+						<input type="text" placeholder="卫衣" />
+					</a>
+					<img src="components/home/img/xx.png" alt="" />
+				</form>
+			</div>-->
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'home',
+		data(){
+			return{
+				arrAllData:[]
+			}
+		},
+		mounted:function(){
+			this.$http.get('static/json/topContent.json').then(function(res){
+				this.arrAllData=res.body.data;
+				console.log(this.arrAllData)
+				
+			})
+		}
+	}
+</script>
+
+<style>
+.home{width: 100%;height: 100%;}
+#home_main{width: 100%;position: absolute;top: 2.5rem;overflow:auto;}
+#home_search{width: 100%;height: 1.9rem;}
+#home_search>form{width: 100%;height: 100%;}
+#home_search>form>img{margin-left:.6rem;width: .7rem;vertical-align: middle;}
+#home_search #logo{width: 1.3rem;}
+#home_search>form>a{width: 11.3rem;height: 1.2rem;margin:.35rem 0 0 .6rem;display:inline-block;}
+#home_search>form>a>input{width: 100%;height: 100%; border: none;border-radius: 5px;outline: none;
+	background:#f4f4f4 url("img/search.png")no-repeat .4rem center ;padding-left: 1.2rem;color: #a9a9a9;background-size: .5rem .5rem;}
+</style>
